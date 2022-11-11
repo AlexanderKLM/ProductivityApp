edf66954a17760010668ff96de30e2d20996ab85
AlexanderKLM <alex57net@mail.ru>
2022-11-12 00:08:22 +0300

    Версия 1

---
+++ app/src/main/java/com/example/productivityapp/ProductivityBlocks.kt
@@ -0,0 +1,5 @@
+package com.example.productivityapp
+
+class ProductivityBlocks (val Name: String, val Description: String, val DeadLine: String) {
+
+}
---
+++ app/src/main/java/com/example/productivityapp/ProductivityBlocksAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.productivityapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductivityBlocksAdapter: RecyclerView.Adapter<ProductivityBlocksAdapter.ProductivityBlocksHolder> {
+
+    class ProductivityBlocksHolder(item: View): RecyclerView.ViewHolder(item) {
+        fun bind(Block: ProductivityBlocks){
+
+        }
+
+    }
+}
